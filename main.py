567edbd1a2f30a511a72bba64ff26d65f3dd4874
ales-sandrito <calivaalessandro@gmail.com>
2024-11-25 21:02:05 +0200

    Improved main.py csv export funcionality

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from alessandro_c_mod1_atsiskaitymas.web_crawler import  web_crawler
+from alessandro_c_mod1_atsiskaitymas.web_crawler import  web_crawler, save_to_csv
 
 if __name__ == "__main__":
   #CSV
-    web_crawler(url="https://www.gintarine.lt/maistas-ir-papildai-sportininkams", timeout=60, output_format="csv")
+    data = web_crawler()
+    save_to_csv(data)
 
+  #DICT
     # data = web_crawler(url="https://www.gintarine.lt/maistas-ir-papildai-sportininkams", timeout=60, output_format="dict")
     # print(data)
